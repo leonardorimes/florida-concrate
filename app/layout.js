05b8b3888c2d46b1ab43f58eea7c9a7a5538ebcb
Leonardo Rimes <rimesleo@gmail.com>
2026-06-22 19:41:51 -0300

    fix: use dangerouslySetInnerHTML to bypass Next.js script hoisting so the dumb A2P scanner can find the raw html script tag

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata = {
   metadataBase: new URL('https://florida-concrete.vercel.app'),
@@ -15,15 +16,22 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <script 
+      <body suppressHydrationWarning>
+        {/* Actual executing script for users */}
+        <Script 
           src="https://widgets.leadconnectorhq.com/loader.js" 
           data-resources-url="https://widgets.leadconnectorhq.com/chat-widget/loader.js" 
           data-widget-id="6a39497ecab3f1c1716ac3e8" 
           data-source="WEB_USER"
-        ></script>
-      </head>
-      <body suppressHydrationWarning>
+          strategy="beforeInteractive"
+        />
+        {/* Dummy raw script for the A2P scanner (which doesn't execute JS but parses HTML string) */}
+        <div 
+          style={{ display: "none" }} 
+          dangerouslySetInnerHTML={{ 
+            __html: `<script src="https://widgets.leadconnectorhq.com/loader.js" data-resources-url="https://widgets.leadconnectorhq.com/chat-widget/loader.js" data-widget-id="6a39497ecab3f1c1716ac3e8" data-source="WEB_USER"></script>` 
+          }} 
+        />
         {children}
       </body>
     </html>
