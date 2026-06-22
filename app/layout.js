e558124fb0555a5699e7900982de3ef459281cb8
Leonardo Rimes <rimesleo@gmail.com>
2026-06-22 19:55:04 -0300

    fix: isolate chat widget to homepage and restore contact form on contactus to pass A2P multiple opt-ins check

--- app/layout.js
+++ app/layout.js
@@ -17,21 +17,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body suppressHydrationWarning>
-        {/* Actual executing script for users */}
-        <Script 
-          src="https://widgets.leadconnectorhq.com/loader.js" 
-          data-resources-url="https://widgets.leadconnectorhq.com/chat-widget/loader.js" 
-          data-widget-id="6a39497ecab3f1c1716ac3e8" 
-          data-source="WEB_USER"
-          strategy="beforeInteractive"
-        />
-        {/* Dummy raw script for the A2P scanner (which doesn't execute JS but parses HTML string) */}
-        <div 
-          style={{ display: "none" }} 
-          dangerouslySetInnerHTML={{ 
-            __html: `<script src="https://widgets.leadconnectorhq.com/loader.js" data-resources-url="https://widgets.leadconnectorhq.com/chat-widget/loader.js" data-widget-id="6a39497ecab3f1c1716ac3e8" data-source="WEB_USER"></script>` 
-          }} 
-        />
         {children}
       </body>
     </html>
