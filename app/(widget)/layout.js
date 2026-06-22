db6e07b8dc9a2360a6514eea81ea27068ca5339a
Leonardo Rimes <rimesleo@gmail.com>
2026-06-22 20:09:19 -0300

    fix: remove duplicate Chat Widget script to resolve Multiple Opt-ins error

--- app/(widget)/layout.js
+++ app/(widget)/layout.js
@@ -7,15 +7,7 @@ export default function WidgetLayout({ children }) {
   return (
     <>
       {/* Hack to force raw HTML script output without div wrappers or Next.js hoisting */}
-      <script dangerouslySetInnerHTML={{ __html: `</script><script src="https://widgets.leadconnectorhq.com/loader.js" data-resources-url="https://widgets.leadconnectorhq.com/chat-widget/loader.js" data-widget-id="6a39497ecab3f1c1716ac3e8" data-source="WEB_USER"></script><script>` }} />
-      {/* Also keep the actual loading script for real users */}
-      <script 
-        src="https://widgets.leadconnectorhq.com/loader.js" 
-        data-resources-url="https://widgets.leadconnectorhq.com/chat-widget/loader.js" 
-        data-widget-id="6a39497ecab3f1c1716ac3e8" 
-        data-source="WEB_USER"
-        async
-      />
+      <script dangerouslySetInnerHTML={{ __html: `</script><script src="https://widgets.leadconnectorhq.com/loader.js" data-resources-url="https://widgets.leadconnectorhq.com/chat-widget/loader.js" data-widget-id="6a39497ecab3f1c1716ac3e8" data-source="WEB_USER" async></script><script>` }} />
       {children}
     </>
   );
